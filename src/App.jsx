51ba354329dbacd9aaaaec5eb6d4cd58b5055818
Manikandan-gopu <manikandank3011@gmail.com>
2024-04-08 00:38:13 +0530

    First_project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+  return  <> 
+  <Greet/>
+  </>
+}
+function Greet(){
+  return(
+    <div>
+      <h1>HELLO REACT</h1>
+    </div>
+  )
+}
+
+export default App;
